c3efcaea2848557e39efcf108a90c1e7e88792d3
ujin-Bang <qkddnwls14@gmail.com>
2022-05-24 23:22:14 +0900

    어댑터가 받아온 재료를 -> 부모에게 전달

--- app/src/main/java/com/start/listview_20220524/adapters/StudentAdapter.kt
+++ app/src/main/java/com/start/listview_20220524/adapters/StudentAdapter.kt
@@ -7,5 +7,5 @@ import com.start.listview_20220524.datas.StudentData
 class StudentAdapter(
     val mContext: Context,
     val resId: Int,
-    val mList: ArrayList<StudentData> ) : ArrayAdapter<StudentData>() {
+    val mList: ArrayList<StudentData> ) : ArrayAdapter<StudentData>( mContext, resId, mList) {
 }
