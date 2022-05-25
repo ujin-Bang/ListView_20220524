3baaa9b32412bea0919e3e1881098f94709285e2
ujin-Bang <qkddnwls14@gmail.com>
2022-05-25 12:10:08 +0900

    액티비티에서 ArrayList 생성 + StudentData들을 담아주는 작업  - 코드로 직접 타이핑 : 임시방편

--- app/src/main/java/com/start/listview_20220524/MainActivity.kt
+++ app/src/main/java/com/start/listview_20220524/MainActivity.kt
@@ -4,13 +4,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.start.listview_20220524.databinding.ActivityMainBinding
+import com.start.listview_20220524.datas.StudentData
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding : ActivityMainBinding
 
+    val mStudentList = ArrayList<StudentData>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        mStudentList.add( StudentData("조경진",1988, "서울시 동대문구") )
+        mStudentList.add( StudentData("김민성", 1998, "서울시 종로구"))
+        mStudentList.add( StudentData("김준기", 1996,  "경기도 남양주시"))
+        mStudentList.add( StudentData("방우진", 1983,  "경기도 고양시"))
+        mStudentList.add( StudentData("이아현", 1996  "서울시 동대문구"))
+        mStudentList.add( StudentData("이지원", 1993,  "서울시 관악구"))
+        mStudentList.add( StudentData("차수나", 1977,  "서울시 성북구"))
+        mStudentList.add( StudentData("김경식", 1992,  "서울시 중랑구"))
     }
 }
